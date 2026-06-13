7e4dcd51ffaee3290fe1252d210abb67836b65fe
Patrice404 <patricecotcho@gmail.com>
2026-06-13 19:32:10 +0200

    Update

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,35 +1,10 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import type { User } from '@campuslink/shared'
-
-const users = ref<User[]>([])
-
-const fetchUsers = async () => {
-  try {
-    const response = await fetch('/api/users')
-    if (!response.ok) throw new Error('Erreur lors de la récupération')
-    
-    users.value = await response.json()
-  } catch (error) {
-    console.error('Erreur:', error)
-  }
-}
-
-onMounted(() => {
-  fetchUsers()
-})
+// Code de la page projets à venir plus tard
 </script>
 
 <template>
   <div>
-    <h1>🏠 Accueil</h1>
-    <h2>Liste des Étudiants :</h2>
-    
-    <ul v-if="users.length > 0">
-      <li v-for="user in users" :key="user.id">
-        <strong>{{ user.name }}</strong> - {{ user.email }}
-      </li>
-    </ul>
-    <p v-else>Chargement...</p>
+    <h1>🚀 Les Projets Tutorés</h1>
+    <p>C'est ici que nous afficherons les projets de la base de données !</p>
   </div>
 </template>
