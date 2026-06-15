9cf9348878af95b5f83dab3a7daa9795b7ec89ab
Patrice404 <patricecotcho@gmail.com>
2026-06-15 13:38:41 +0200

    Fix bug

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,7 +3,6 @@ import CampusSelectView from '../views/CampusSelectView.vue' // <-- Nouvelle pag
 import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
-import UsersView from '../views/UsersView.vue'
 import RegisterView from '../views/RegisterView.vue' 
 import ProfileView from '../views/ProfileView.vue'
 
@@ -40,11 +39,6 @@ const router = createRouter({
       path: '/projets',
       name: 'projects',
       component: ProjectsView
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: UsersView
     },
     // La page profile utilisateur
     {
