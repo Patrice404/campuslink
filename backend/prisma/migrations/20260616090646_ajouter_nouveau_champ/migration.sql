7f5c2156e87eb63131b5e5f01094c72ef77f65ba
Patrice404 <patricecotcho@gmail.com>
2026-06-16 11:11:58 +0200

    Data test for database

---
+++ backend/prisma/migrations/20260616090646_ajouter_nouveau_champ/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `texte` on the `AnnonceProjet` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "AnnonceProjet" DROP COLUMN "texte";
